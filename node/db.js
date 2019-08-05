3d5715a628716fa4cadebefdd414bfb2bbab59fe
Hoang Duy Phuc <hoangduyphuc9x@gmail.com>
2019-08-05 17:47:00 +0700

    update DapNews and MongoDB Database Name

--- node/db.js
+++ node/db.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 MONGODB_HOST = "localhost";
 MONGODB_PORT = 27017;
-MONGODB_DATABASE = "OFFICIAL_DATABASE";
+MONGODB_DATABASE = "OFFDB";
 
 mongoose.connect(`mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`,{useNewUrlParser:true});
